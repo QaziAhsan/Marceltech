befb15c0e1d7501f0dc37d846039071290141b84
QaziAhsan <iamahsan96@gmail.com>
2023-01-23 20:16:08 +0500

    commented code

--- assets/js/main.js
+++ assets/js/main.js
@@ -423,20 +423,63 @@ $(".anim-skewinup").each(function () {
   );
 });
 
+// stretch in-up
+$(".anim-stretchinup").each(function () {
+  let tl_StretchInUp = gsap.timeline({
+    scrollTrigger: {
+      trigger: this,
+      start: "top bottom",
+      markers: false,
+    },
+  });
 
-	// stretch in-up
-	$(".anim-stretchinup").each(function() {
-		let tl_StretchInUp = gsap.timeline({
-			scrollTrigger: {
-				trigger: this,
-				start: "top bottom",
-				markers: false
-			}
-		});
+  tl_StretchInUp.from(
+    this,
+    {
+      duration: 2,
+      autoAlpha: 0,
+      y: 100,
+      scaleY: 1.4,
+      transformOrigin: "top",
+      ease: Expo.easeOut,
+      clearProps: "all",
+    },
+    "+=0.2"
+  );
+});
 
-		tl_StretchInUp.from(this, { duration: 2, autoAlpha: 0, y: 100, scaleY: 1.4, transformOrigin: "top", ease: Expo.easeOut, clearProps:"all" }, "+=0.2");
-	});
+// zoom in
+// $(".anim-zoomin").each(function () {
+//   // Add wrap <div>.
+//   $(this).wrap('<div class="anim-zoomin-wrap"></div>');
 
+//   // Add overflow hidden.
+//   $(".anim-zoomin-wrap").css({ overflow: "hidden" });
+
+//   var $this = $(this);
+//   var $asiWrap = $this.parents(".anim-zoomin-wrap");
+
+//   let tl_ZoomIn = gsap.timeline({
+//     scrollTrigger: {
+//       trigger: $asiWrap,
+//       start: "top 90%",
+//       markers: false,
+//       onEnter: () => animZoomInRefresh(),
+//     },
+//   });
+//   tl_ZoomIn.from($this, {
+//     duration: 1.5,
+//     autoAlpha: 0,
+//     scale: 1.2,
+//     ease: Power2.easeOut,
+//     clearProps: "all",
+//   });
+
+//   // Refresh start/end positions on enter.
+//   function animZoomInRefresh() {
+//     ScrollTrigger.refresh();
+//   }
+// });
 
 // Page header elements scrolling effects:
 
@@ -584,4 +627,231 @@ $(document).mousemove(function () {
   gsap.to("#magic-cursor", { duration: 0.3, autoAlpha: 1 });
 });
 
+// =======================================================================================
+// Portfolio slider (full screen slider)
+// Source: https://swiperjs.com/
+// =======================================================================================
 
+if ($(".tt-portfolio-slider").length) {
+  $(".tt-portfolio-slider").each(function () {
+    var $ttPortfolioSlider = $(this);
+
+    // Data attributes
+    // ================
+    var $dataMousewheel = $ttPortfolioSlider.data("mousewheel");
+    var $dataKeyboard = $ttPortfolioSlider.data("keyboard");
+    var $dataSimulateTouch = $ttPortfolioSlider.data("simulate-touch");
+    var $dataGrabCursor = $ttPortfolioSlider.data("grab-cursor");
+    var $dataAutoplay = $ttPortfolioSlider.data("autoplay")
+      ? { delay: $ttPortfolioSlider.data("autoplay") }
+      : $ttPortfolioSlider.data("autoplay");
+    var $dataLoop = $ttPortfolioSlider.data("loop")
+      ? { loopedSlides: 100 }
+      : $ttPortfolioSlider.data("loop"); // Not recommended!
+
+    if ($ttPortfolioSlider.is("[data-speed]")) {
+      var $dataSpeed = $ttPortfolioSlider.data("speed");
+    } else {
+      var $dataSpeed = 900; // by default
+    }
+
+    if ($ttPortfolioSlider.is("[data-pagination-type]")) {
+      var $dataPaginationType = $ttPortfolioSlider.data("pagination-type");
+    } else {
+      var $dataPaginationType = "fraction"; // by default (bullets/fraction/progressbar)
+    }
+
+    // Init Swiper
+    // =============
+    var $ttPortfolioSliderSwiper = new Swiper(
+      $ttPortfolioSlider.find(".swiper")[0],
+      {
+        // Parameters
+        direction: "vertical",
+        effect: "slide",
+        speed: 600, // slider speed for smaller screens (when window width is 1024px or smaller)
+        parallax: true,
+        resistanceRatio: 0,
+        longSwipesRatio: 0.02,
+        preloadImages: false, // Needed for lazy loading
+        preventInteractionOnTransition: true, // No actions during transition
+        autoplay: $dataAutoplay,
+        // mousewheel: $dataMousewheel,
+        keyboard: $dataKeyboard,
+        simulateTouch: $dataSimulateTouch,
+        grabCursor: $dataGrabCursor,
+        loop: $dataLoop, // Not recommended!
+
+        breakpoints: {
+          // when window width is 1025px or larger
+          1025: {
+            speed: $dataSpeed,
+          },
+        },
+
+        // Lazy loading
+        lazy: {
+          loadPrevNext: true,
+          loadOnTransitionStart: true,
+        },
+
+        // Navigation arrows
+        navigation: {
+          nextEl: $ttPortfolioSlider.find(".tt-ps-nav-arrow-next")[0],
+          prevEl: $ttPortfolioSlider.find(".tt-ps-nav-arrow-prev")[0],
+          disabledClass: "tt-ps-nav-arrow-disabled",
+        },
+
+        // Pagination
+        pagination: {
+          el: $ttPortfolioSlider.find(".tt-ps-nav-pagination")[0],
+          type: $dataPaginationType,
+          modifierClass: "tt-ps-nav-pagination-",
+          dynamicBullets: true,
+          dynamicMainBullets: 1,
+          clickable: true,
+        },
+
+        // Events
+        on: {
+          init: function () {
+            var $this = this;
+            var $slideActive = $($this.slides[$this.activeIndex]);
+
+            // Play video on load
+            $slideActive.find("video").each(function () {
+              $(this).get(0).play();
+            });
+
+            // Portfolio slider caption on load
+            // ---------------------------------
+            // Portfolio slider caption title (if contains link or not)
+            if (
+              $ttPortfolioSlider.find(".tt-ps-caption-title").find("a").length
+            ) {
+              $ttPortfolioSlider
+                .find(".tt-ps-caption-title a")
+                .text($slideActive.attr("data-title"));
+              $ttPortfolioSlider
+                .find(".tt-ps-caption-title a")
+                .attr("href", $slideActive.attr("data-url"));
+            } else {
+              $ttPortfolioSlider
+                .find(".tt-ps-caption-title")
+                .text($slideActive.attr("data-title"));
+            }
+
+            // Portfolio slider caption category on load
+            $ttPortfolioSlider
+              .find(".tt-ps-caption-category")
+              .text($slideActive.attr("data-category"));
+
+            // If slider image is light on load
+            setTimeout(function () {
+              if ($slideActive.hasClass("psi-image-is-light")) {
+                $("body").addClass("psi-light-image-on");
+              } else {
+                $("body").removeClass("psi-light-image-on");
+              }
+            }, 400);
+          },
+
+          transitionStart: function () {
+            var $this = this;
+            var $slideActive = $($this.slides[$this.activeIndex]);
+
+            // If slider image is light
+            setTimeout(function () {
+              if ($slideActive.hasClass("psi-image-is-light")) {
+                $("body").addClass("psi-light-image-on");
+              } else {
+                $("body").removeClass("psi-light-image-on");
+              }
+            }, 400);
+
+            // Play video
+            $slideActive.find("video").each(function () {
+              $(this).get(0).play();
+            });
+
+            // Animate portfolio slider caption
+            gsap.fromTo(
+              $ttPortfolioSlider.find(".tt-psc-elem"),
+              { autoAlpha: 1, y: 0 },
+              {
+                duration: 0.25,
+                autoAlpha: 0,
+                y: -30,
+                stagger: 0.15,
+                ease: Power1.easeIn,
+              }
+            );
+          },
+
+          transitionEnd: function () {
+            var $this = this;
+            var $slideActive = $($this.slides[$this.activeIndex]);
+
+            // Pause video
+            $slideActive
+              .prevAll()
+              .find("video")
+              .each(function () {
+                $(this).get(0).pause();
+              });
+            $slideActive
+              .nextAll()
+              .find("video")
+              .each(function () {
+                $(this).get(0).pause();
+              });
+
+            // Portfolio slider caption
+            // -------------------------
+            // Portfolio slider caption title (if contains link or not)
+            if (
+              $ttPortfolioSlider.find(".tt-ps-caption-title").find("a").length
+            ) {
+              $ttPortfolioSlider
+                .find(".tt-ps-caption-title a")
+                .text($slideActive.attr("data-title"));
+              $ttPortfolioSlider
+                .find(".tt-ps-caption-title a")
+                .attr("href", $slideActive.attr("data-url"));
+            } else {
+              $ttPortfolioSlider
+                .find(".tt-ps-caption-title")
+                .text($slideActive.attr("data-title"));
+            }
+
+            // Portfolio slider caption category
+            $ttPortfolioSlider
+              .find(".tt-ps-caption-category")
+              .text($slideActive.attr("data-category"));
+
+            // Animate portfolio slider caption
+            gsap.fromTo(
+              $ttPortfolioSlider.find(".tt-psc-elem"),
+              { autoAlpha: 0, y: 30 },
+              {
+                duration: 0.25,
+                autoAlpha: 1,
+                y: 0,
+                stagger: 0.15,
+                ease: Power1.easeOut,
+              }
+            );
+          },
+        },
+      }
+    );
+
+   
+  });
+}
+var swiper = new Swiper(".testimonial-swipper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
